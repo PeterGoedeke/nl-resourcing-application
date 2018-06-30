a5e674122d7491795e699ef2d4cb6ae61309221a
PeterGoedeke <peterbgoedeke@gmail.com>
2018-06-30 14:28:03 +1200

    Draggable, EmployeeSlot Rework Framework

--- main3.js
+++ main3.js
@@ -54,7 +54,7 @@ let draggingInterface = (function() {
                         currentlyDragging.drag(event, direction)
                         if(direction == 'right') {
                             sq.contentPane.scrollLeft += 6
-                            if(sq.getVisibleTimeBlockRange()[1] > parseInt(sq.topAxisContainer.lastChild.textContent) - 5) {
+                            if(sq.getVisibleTimeBlockRange()[1] == parseInt(sq.topAxisContainer.lastChild.textContent)) {
                                 sm.appendTimeBlock(parseInt(sq.topAxisContainer.lastChild.textContent) + 1)
                                 sq.positioner.style.width = sq.getTimeBlockWidth() * sq.topAxisContainer.childNodes.length + 'px'
                             }
@@ -85,18 +85,40 @@ let draggingInterface = (function() {
     }
 })()
 
+let draggable = {
+    drag(event, side) {
+        sq.contentPane.style.cursor = 'pointer'
+        if(side == 'left') {
+            this.display.style.left = sq.getCursorXLocation(event.pageX) + 'px'
+            this.display.style.width = getXLocationFromID(this.endDate) - sq.getCursorXLocation(event.pageX) + 'px'
+        }
+        else if(side == 'right') {
+            this.display.style.width = sq.getCursorXLocation(event.pageX) - parseInt(this.display.style.left) + 'px'
+        }
+    },
+    stopDrag(direction) {
+        sq.contentPane.style.cursor = 'auto'
+        this.startDate = sq.getNearestTimeBlock(parseInt(this.display.style.left))
+        this.endDate = sq.getNearestTimeBlock(parseInt(this.display.style.left) + parseInt(this.display.style.width))
+        if(direction == 'left' && this.startDate == this.endDate) this.startDate --
+        else if(direction == 'right' && this.startDate == this.endDate) this.endDate ++
+        this.updateDisplay()
+        console.log(this.startDate, this.endDate)
+    }
+}
+
 function calculateCursors() {
-    let cursorPastLeftSide = sq.getCursorXLocation(event.pageX) > parseInt(this.projectDisplay.style.left)
-    let cursorCloseToLeftSide = sq.getCursorXLocation(event.pageX) < parseInt(this.projectDisplay.style.left) + 10
+    let cursorPastLeftSide = sq.getCursorXLocation(event.pageX) > parseInt(this.display.style.left)
+    let cursorCloseToLeftSide = sq.getCursorXLocation(event.pageX) < parseInt(this.display.style.left) + 10
     
-    let cursorBeforeRightSide = sq.getCursorXLocation(event.pageX) < parseInt(this.projectDisplay.style.left) + parseInt(this.projectDisplay.style.width)
-    let cursorCloseToRightSide = sq.getCursorXLocation(event.pageX) > parseInt(this.projectDisplay.style.left) + parseInt(this.projectDisplay.style.width) - 10
+    let cursorBeforeRightSide = sq.getCursorXLocation(event.pageX) < parseInt(this.display.style.left) + parseInt(this.display.style.width)
+    let cursorCloseToRightSide = sq.getCursorXLocation(event.pageX) > parseInt(this.display.style.left) + parseInt(this.display.style.width) - 10
     return {cursorPastLeftSide, cursorCloseToLeftSide, cursorBeforeRightSide, cursorCloseToRightSide}
 }
 
 let projectProto = {
     initDisplay() {
-        this.projectDisplay.addEventListener('mousedown', (event) => {
+        this.display.addEventListener('mousedown', (event) => {
             const {
                 cursorPastLeftSide,
                 cursorCloseToLeftSide,
@@ -111,7 +133,7 @@ let projectProto = {
             }
         })
 
-        this.projectDisplay.addEventListener('mousemove', (event) => {
+        this.display.addEventListener('mousemove', (event) => {
             const {
                 cursorPastLeftSide,
                 cursorCloseToLeftSide,
@@ -119,67 +141,78 @@ let projectProto = {
                 cursorCloseToRightSide
             } = calculateCursors.call(this)
             if(cursorPastLeftSide && cursorCloseToLeftSide) {
-                this.projectDisplay.style.cursor = 'pointer'
+                this.display.style.cursor = 'pointer'
             }
             else if(cursorBeforeRightSide && cursorCloseToRightSide) {
-                this.projectDisplay.style.cursor = 'pointer'
+                this.display.style.cursor = 'pointer'
             }
             else {
-                this.projectDisplay.style.cursor = 'auto'
+                this.display.style.cursor = 'auto'
             }
         })
 
-        sq.contentPane.appendChild(this.projectDisplay)
+        this.createEmployeeSlotButton.addEventListener('mouseup', (event) => {
+
+        })
+
+        this.display.appendChild(this.createEmployeeSlotButton)
+        sq.contentPane.appendChild(this.display)
         document.querySelector('.leftSidebar').insertBefore(this.projectLabel, sq.createProjectButton)
         this.updateDisplay()
     },
     updateDisplay() {
-        this.projectDisplay.style.left = getXLocationFromID(this.startDate) + 'px'
-        this.projectDisplay.style.width = getXLocationFromID(this.endDate) - getXLocationFromID(this.startDate) + 'px'
+        this.display.style.left = getXLocationFromID(this.startDate) + 'px'
+        this.display.style.width = getXLocationFromID(this.endDate) - getXLocationFromID(this.startDate) + 'px'
     },
     rename(newName) {
         this.name = newName
         this.projectLabel.textContent = this.name
-    },
-    drag(event, side) {
-        sq.contentPane.style.cursor = 'pointer'
-        if(side == 'left') {
-            this.projectDisplay.style.left = sq.getCursorXLocation(event.pageX) + 'px'
-            this.projectDisplay.style.width = getXLocationFromID(this.endDate) - sq.getCursorXLocation(event.pageX) + 'px'
-        }
-        else if(side == 'right') {
-            this.projectDisplay.style.width = sq.getCursorXLocation(event.pageX) - parseInt(this.projectDisplay.style.left) + 'px'
-        }
-    },
-    stopDrag(direction) {
-        sq.contentPane.style.cursor = 'auto'
-        this.startDate = sq.getNearestTimeBlock(parseInt(this.projectDisplay.style.left))
-        this.endDate = sq.getNearestTimeBlock(parseInt(this.projectDisplay.style.left) + parseInt(this.projectDisplay.style.width))
-        if(direction == 'left' && this.startDate == this.endDate) this.startDate --
-        else if(direction == 'right' && this.startDate == this.endDate) this.endDate ++
-        this.updateDisplay()
-        console.log(this.startDate, this.endDate)
     }
 }
 
 function createProject(name, group, security) {
     let [startDate, endDate] = sq.getVisibleTimeBlockRange(true)
-    let projectDisplay = document.createElement('div')
-    projectDisplay.className = 'project'
+    let display = document.createElement('div')
+    display.className = 'project'
+    
     let projectLabel = document.createElement('div')
     projectLabel.className = 'projectLabel'
     projectLabel.textContent = name
-    let dragging = false
 
+    let createEmployeeSlotButton = document.createElement('div')
+    createEmployeeSlotButton.className = 'createEmployeeSlot'
+    createEmployeeSlotButton.textContent = '+'
+    //createEmployeeSlotButton.style.left =
+
+    let dragging = false
     let project = Object.assign(
         Object.create(projectProto),
-        {projectDisplay, projectLabel, dragging},
-        {name, group, security, startDate, endDate}
+        draggable,
+        {display, projectLabel, createEmployeeSlotButton, dragging,
+        name, group, security, startDate, endDate}
     )
     project.initDisplay()
     state.registerProject(project)
     return project
 }
+
+let employeeSlotProto = {
+    assignEmployee(employee) {
+        this.assignedEmployee = employee
+        this.assignedEmployee.workload[workloadInformation] = this[workloadInformation]
+    },
+    removeEmployee() {
+        if(this.employee.name) {
+            delete this.employee.workload[workloadInformation]
+            this.employee = null
+        }
+    },
+    reallocateWorkload(newWorkload) {
+        this[workloadInformation].length = 0
+        this[workloadInformation].push(...newWorkload)
+    }
+}
+
 function createEmployeeSlot(hostProject, employeeType) {
     let workloadInformation = Symbol('workload information')
     let workload = Object.create(null)
@@ -187,24 +220,14 @@ function createEmployeeSlot(hostProject, employeeType) {
 
     let assignedEmployee = 'Empty'
 
-    return {
-        hostProject, employeeType, startDate: hostProject.startDate, endDate: hostProject.endDate, assignedEmployee,
-        [workloadInformation]: workload,
-        assignEmployee(employee) {
-            this.assignedEmployee = employee
-            this.assignedEmployee.workload[workloadInformation] = this[workloadInformation]
-        },
-        removeEmployee() {
-            if(this.employee.name) {
-                delete this.employee.workload[workloadInformation]
-                this.employee = null
-            }
-        },
-        reallocateWorkload(newWorkload) {
-            this[workloadInformation].length = 0
-            this[workloadInformation].push(...newWorkload)
-        }
-    }
+    let employeeSlot = Object.assign(
+            Object.create(employeeSlotProto,
+            {hostProject, employeeType, startDate: hostProject.startDate, endDate: hostProject.endDate, assignedEmployee,
+            [workloadInformation]: workload}
+        )
+    )
+
+    return employeeSlot
 }
 
 function createEmployee() {
