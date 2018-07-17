e9ae76a6f1aae442a08f97424f9bb46cd6076f2a
PeterGoedeke <peterbgoedeke@gmail.com>
2018-07-17 22:43:50 +1200

    Fixed Critical Bugs in Type Pane Switching

--- types.js
+++ types.js
@@ -9,7 +9,7 @@ const employeeTypeProto = {
         })
         this.display.addEventListener('mouseup', event => {
             state.setVisibleType(this)
-            
+
         })
         this.display.addEventListener('dblclick', event => {
             let displayInput = document.createElement('input')
@@ -17,10 +17,24 @@ const employeeTypeProto = {
             displayInput.className = 'employeeTypeInput'
             displayInput.value = this.type
             displayInput.addEventListener('blur', event => {
-                this.type = displayInput.value
-                this.display.removeChild(displayInput)
-                this.display.appendChild(document.createTextNode(this.type.toUpperCase()))
-                state.setVisibleType(state.visibleType)
+                if(displayInput.value != this.type) {
+                    displayInput.value = displayInput.value.toUpperCase()
+                    state.projects.forEach(project => {
+                        project.employeeSlots[displayInput.value] = project.employeeSlots[this.type].slice(0)
+                        project.employeeSlots[displayInput.value].forEach(employeeSlot => {
+                            employeeSlot.employeeType = displayInput.value
+                        })
+                        delete project.employeeSlots[this.type]
+                    })
+                    leave.leaveSlots[displayInput.value] = leave.leaveSlots[this.type].slice(0)
+                    leave.leaveSlots[displayInput.value].forEach(leaveSlot => {
+                        leaveSlot.employeeType = displayInput.value
+                    })
+                    this.type = displayInput.value
+                    this.display.removeChild(displayInput)
+                    this.display.appendChild(document.createTextNode(this.type.toUpperCase()))
+                    state.setVisibleType(state.visibleType)
+                }
             })
 
             this.display.removeChild(this.display.firstChild)
