aa5f36a9c454d0adff7882aa0cc5434b73582da8
PeterGoedeke <peterbgoedeke@gmail.com>
2018-07-23 19:11:05 +1200

    Fixed Bug Where CreateEmployeeSlotButton Would Position Incorrectly if There Were no EmployeeSlots

--- projects.js
+++ projects.js
@@ -46,17 +46,17 @@ let projectProto = {
         sm.updateVerticalDisplay()
     },
     updateCreateEmployeeSlotButton() {
+        const projectRight = parseInt(this.display.style.left) + parseInt(this.display.style.width)
         if(this.employeeSlots[state.visibleType.type].length >= 1) {
             const lastEmployeeSlot = this.employeeSlots[state.visibleType.type][this.employeeSlots[state.visibleType.type].length - 1]
             const lastEmployeeSlotRight = parseInt(lastEmployeeSlot.display.style.left) + parseInt(lastEmployeeSlot.display.style.width)
-            const projectRight = parseInt(this.display.style.left) + parseInt(this.display.style.width)
             if(lastEmployeeSlotRight > projectRight) {
                 this.createEmployeeSlotButton.style.left = lastEmployeeSlotRight + 15 * zoom.scale + 2.5 + 'px'
             }
             else {
                 this.createEmployeeSlotButton.style.left = projectRight + 15 * zoom.scale + 2.5 + 'px'
             }
-        }
+        } else this.createEmployeeSlotButton.style.left = projectRight + 15 * zoom.scale + 2.5 + 'px'
     },
     showVisibleTypes() {
         for(let type in this.employeeSlots) this.employeeSlots[type].forEach(employeeSlot => {
