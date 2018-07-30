80308c33a3c1e7814ae81c9dd2502a7a7d69e657
PeterGoedeke <peterbgoedeke@gmail.com>
2018-07-30 19:39:08 +1200

    Added Tabbing to First Project

--- frameworks/tab.js
+++ frameworks/tab.js
@@ -9,6 +9,15 @@ const tab = (function() {
                     if(indexOfProjectInProjects == state.projects.length - 1) {
                         if(leave.leaveSlots[state.visibleType.type].length > 0) leave.leaveSlots[state.visibleType.type][0].label.focus()
                         else if(state.getVisibleEmployees().length > 0) state.getVisibleEmployees()[0].label.focus()
+    function findFirstProject() {
+        for(const project of state.projects) {
+            if(project.employeeSlots[state.visibleType.type].length > 0) {
+                project.employeeSlots[state.visibleType.type][0].label.focus()
+                return true
+            }
+        }
+        return false
+    }
                     }
                     else state.projects[indexOfProjectInProjects + 1].employeeSlots[state.visibleType.type][0].label.focus()
         right(focused, hostObject) {
