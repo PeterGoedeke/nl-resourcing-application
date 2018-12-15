c0ccbe16a6534165e9ae11b168738be0f973eb42
PeterGoedeke <peterbgoedeke@gmail.com>
2018-12-15 20:54:16 +1300

    Tested Limited Width Document Flow

--- test.js
+++ test.js
@@ -52,9 +52,7 @@ const random = {
     color() {
         const characters = '0123456789ABCDEF'
         let color = '#'
-        for(let i = 0; i < 6; i++) {
-            color += characters[Math.floor(Math.random() * 16)]
-        }
+        for(let i = 0; i < 6; i++) color += characters[Math.floor(Math.random() * 16)]
         return color
     },
     number(lower, upper) {
