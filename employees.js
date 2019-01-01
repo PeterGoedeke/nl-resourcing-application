b284437feba3190925fcd5de3cc24c1870a381e3
PeterGoedeke <peterbgoedeke@gmail.com>
2019-01-01 22:56:33 +1300

    Added Employee Name Changing

--- employees.js
+++ employees.js
@@ -6,17 +6,27 @@ const employeeProto = {
         this.body.style.width = columns.applicationWidth + 'px'
         this.container.style.width = columns.applicationWidth + columns.sidebarWidth + 'px'
 
+        this.label.addEventListener('click', event => {
+            this.label.style.height = columns.rowHeight + 'px'
+            inputify(this.label, newLabel => {
+                this.label.innerHTML = newLabel
+                this.name = newLabel
+                this.label.style.height = 'initial'
+            })
+        })
+
         const snapshotTotalWorkload = this.totalWorkload
         for(const key in snapshotTotalWorkload) {
             const cell = createCell(snapshotTotalWorkload[key])
             this.body.appendChild(cell)
             this.cells[key] = cell
         }
-        console.log(this.totalWorkload)
 
         this.label.textContent = this.name || 'Unnamed'
-        x = this
         return this.container
+    },
+    init() {
+        employees.list.push(this)
     },
     showVisible() {
 
@@ -27,7 +37,6 @@ const employeeProto = {
     get totalWorkload() {
         let totalWorkload = {}
         this.workload.forEach(workload => {
-            console.log('ye')
             for(const key in workload) totalWorkload[key] = totalWorkload[key] + Number(workload[key]) || Number(workload[key])
         })
         for(let i = columns.baseID; i < columns.endID; i++) {
@@ -36,6 +45,10 @@ const employeeProto = {
         }
         return totalWorkload
     }
+}
+
+const employees = {
+    list: []
 }
 
 function createEmployee(details) {
@@ -55,6 +68,6 @@ newEmployeeButton.addEventListener('click', event => {
     const newEmployee = createEmployee()
     const container = newEmployee.batchLoad()
     newEmployee.showVisible()
+    newEmployee.init()
     document.body.insertBefore(container, employeeAreaSeparator)
-    console.log(newEmployee, container)
 })
