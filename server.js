e6d690f24d587432e8438982059c47d9cb1cb791
PeterGoedeke <peterbgoedeke@gmail.com>
2019-02-05 13:18:11 +1300

    JS Served

--- server.js
+++ server.js
@@ -10,6 +10,12 @@ http.createServer(function(req, res) {
     
             fileStream.pipe(res)
         }
+        else if(req.url.match(/.js$/)) {
+            let fileStream = fs.createReadStream(path.join(__dirname, req.url), 'utf-8')
+            res.writeHead(200, {'Content-Type': 'text/js'})
+    
+            fileStream.pipe(res)
+        }
             fs.readFile('./index.html', 'utf-8', function(err, data) {
                 res.writeHead(200, {'Content-Type': 'text/html'})
                 res.end(data)
