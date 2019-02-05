a08e6a3b07a1826a78bfec897134738bb4e8cfe5
PeterGoedeke <peterbgoedeke@gmail.com>
2019-02-05 13:19:14 +1300

    Client Loading Added

--- load.js
+++ load.js
@@ -1,43 +1,32 @@
-const fs = require('fs')
+
 
 const projectFragment = document.createDocumentFragment()
 const interiorsProjectFragment = document.createDocumentFragment()
 const employeeFragment = document.createDocumentFragment()
 const interiorsEmployeeFragment = document.createDocumentFragment()
 
-function checkFiles() {
-    return Promise.all([
-        new Promise(resolve => fs.exists('./data/projects.json', exists => resolve(exists))),
-        new Promise(resolve => fs.exists('./data/employees.json', exists => resolve(exists))),
-        new Promise(resolve => fs.exists('./data/sheets.json', exists => resolve(exists))),
-    ])
-}
-function loadFiles() {
-    return Promise.all([
-        new Promise((resolve, reject) => fs.readFile('./data/sheets.json', 'utf8', (err, data) => {
-            if(err) reject(err)
-            resolve(JSON.parse(data))
-        })),
-        new Promise((resolve, reject) => fs.readFile('./data/projects.json', 'utf8', (err, data) => {
-            if(err) reject(err)
-            resolve(JSON.parse(data))
-        })),
-        new Promise((resolve, reject) => fs.readFile('./data/employees.json', 'utf8', (err, data) => {
-            if(err) reject(err)
-            resolve(JSON.parse(data))
-        }))
-    ])
+function makeFileRequest() {
+    const http = new XMLHttpRequest()
+    const url = window.location.href + '/main'
+    http.open('GET', url)
+    http.send()
+
+    return new Promise(function(resolve) {
+        http.onreadystatechange = function() {
+            if(this.readyState == 4) resolve({status: this.status, data: http.responseText})
+        }
+    })
 }
 
 async function load() {
-    const exists = await checkFiles()
-    if(exists.every(value => value)) {
-        const data = await loadFiles()
+    const res = await makeFileRequest()
 
+    if(res.status == 200) {
+        const data = JSON.parse(res.data)
         const sheetsData = data[0]
         sheetsData.forEach((type, i) => sheets.create(type, i == 0 ? true : false))
         sheets.visible = sheetsData[0]
-
+    
         const employeeData = data[2]
         employeeData.forEach(employee => {
             const newEmployee = createEmployee(employee)
@@ -45,20 +34,24 @@ async function load() {
             if(newEmployee.interiors && newEmployee.type == sheets.visible) interiorsEmployeeFragment.appendChild(container)
             else if(newEmployee.type == sheets.visible) employeeFragment.appendChild(container)
         })
-
+    
         const projectData = data[1]
         projectData.forEach(project => {
             const newProject = createProject(project)
             newProject.init()
-
+    
             const container = newProject.batchLoad()
             if(newProject.interiors) interiorsProjectFragment.appendChild(container)
             else projectFragment.appendChild(container)
         })
-
+    
         insertAfter(projectFragment, projectAreaSeparator)
         insertAfter(interiorsProjectFragment, interiorsProjectAreaSeparator)
         insertAfter(employeeFragment, employeeAreaSeparator)
         insertAfter(interiorsEmployeeFragment, interiorsEmployeeAreaSeparator)
+    } else {
+        console.log('hmm')
     }
 }
+
+load()
