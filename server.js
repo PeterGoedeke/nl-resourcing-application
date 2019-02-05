0bcc85fca4934ec7e1d287bdc5f2d246ecdc86e2
PeterGoedeke <peterbgoedeke@gmail.com>
2019-02-05 13:19:48 +1300

    Server Response to Client Loading

--- server.js
+++ server.js
@@ -22,9 +22,57 @@ http.createServer(function(req, res) {
     
             fileStream.pipe(res)
         }
+        else if(req.url.match(/main$/)) {
+            checkFiles().then(function(value) {
+                if(checkFiles().every)
+                console.log('hmm')
+                loadFiles().then(function(value) {
+                    res.writeHead(200, {'Content-Type': 'text/js'})
+                    res.end(JSON.stringify(value))
+                }, function(err) { throw err })
+            }, createFiles)
+        }
+        else {
             fs.readFile('./index.html', 'utf-8', function(err, data) {
                 res.writeHead(200, {'Content-Type': 'text/html'})
                 res.end(data)
             })
     }
 }).listen(3000)
+function checkFiles() {
+    return Promise.all([
+        new Promise(resolve => fs.exists('./data/projects.json', exists => resolve(exists))),
+        new Promise(resolve => fs.exists('./data/employees.json', exists => resolve(exists))),
+        new Promise(resolve => fs.exists('./data/sheets.json', exists => resolve(exists))),
+    ])
+}
+function loadFiles() {
+    return Promise.all([
+        new Promise((resolve, reject) => fs.readFile('./data/sheets.json', 'utf8', (err, data) => {
+            if(err) reject(err)
+            resolve(JSON.parse(data))
+        })),
+        new Promise((resolve, reject) => fs.readFile('./data/projects.json', 'utf8', (err, data) => {
+            if(err) reject(err)
+            resolve(JSON.parse(data))
+        })),
+        new Promise((resolve, reject) => fs.readFile('./data/employees.json', 'utf8', (err, data) => {
+            if(err) reject(err)
+            resolve(JSON.parse(data))
+        }))
+    ])
+}
+
+function createFiles() {
+    fs.writeFile('./data/projects.json', '', 'utf8', function(err) {
+        if(err) throw err
+    })
+    fs.writeFile('./data/employees.json', '', 'utf8', function(err) {
+        if(err) throw err
+    })
+    fs.writeFile('./data/sheets.json', '', 'utf8', function(err) {
+        if(err) throw err
+    })
+
+    console.log('did it ya boy')
+}
