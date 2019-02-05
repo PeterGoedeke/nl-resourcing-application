8fd7314b9610dd6169b2efe23fc546f573c1f487
PeterGoedeke <peterbgoedeke@gmail.com>
2019-02-05 13:18:04 +1300

    CSS Served

--- server.js
+++ server.js
@@ -4,6 +4,12 @@ const path = require('path')
 
 http.createServer(function(req, res) {
     if(req.method == 'GET') {
+        if(req.url.match(/.css$/)) {
+            let fileStream = fs.createReadStream(path.join(__dirname, req.url), 'utf-8')
+            res.writeHead(200, {'Content-Type': 'text/css'})
+    
+            fileStream.pipe(res)
+        }
             fs.readFile('./index.html', 'utf-8', function(err, data) {
                 res.writeHead(200, {'Content-Type': 'text/html'})
                 res.end(data)
