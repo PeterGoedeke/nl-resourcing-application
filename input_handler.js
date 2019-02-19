0a837abcb7d0a1907d5b4fc27350b80ae16c7e27
PeterGoedeke <peterbgoedeke@gmail.com>
2019-02-19 15:42:33 +1300

    Added Framework for Keyboard Navigation

--- input_handler.js
+++ input_handler.js
@@ -28,6 +28,33 @@ function inputify(element, cb, setText = true) {
     }, 0)
 }
 
+const DIRECTIONS = Object.freeze({
+    left: 37, up: 38, right: 39, down: 40
+})
+
+
+function inputifyNav(element, cb, navigate, database = undefined) {
+    function testNavigation(event) {
+        switch(event.which) {
+            case DIRECTIONS.left:
+            if(event.target.selectionStart != 0) break
+            case DIRECTIONS.right:
+            if(event.which == DIRECTIONS.right && event.target.selectionStart != event.target.value.length && !(event.target.selectionStart == 0 && event.target.selectionEnd == event.target.value.length)) break
+            case DIRECTIONS.up:
+            case DIRECTIONS.down:
+            navigate(event.which)
+        }
+    }
+    function clear() {
+        element.removeEventListener('keydown', testNavigation)
+    }
+
+    if(database) inputifyAutocomplete(element, cb, database)
+    else inputify(element, cb)
+    element.addEventListener('keydown', testNavigation)
+    element.querySelectorAll('input').forEach(input => input.addEventListener('blur', clear))
+}
+
 function inputifyAutocomplete(element, cb, database) {
     database = database.sort()
     let input = document.createElement('input')
